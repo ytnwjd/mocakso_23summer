aca3b021a3267b86c717d19e76ab9bb63bd36c30
sujeong <ysj120808@gmail.com>
2023-07-08 10:39:05 +0900

    Add python_3일차

---
+++ BaekJoon_python/python_3일차.py
@@ -0,0 +1,37 @@
+## 그냥 문제풀이 ..
+
+# <10797>
+# day = int(input())
+# num_list = list(map(int, input().split()))
+# cnt = 0
+# for c in num_list:
+#     if (day==c):
+#         cnt += 1
+# print(cnt)
+
+# <2440>
+# num = int(input())
+# for i in range(num, 0, -1):
+#     print("*"*(i))
+
+# <2441>
+# num = int(input())
+# for i in range(num):
+#     print(" "*(i) + "*"*(num-i))
+
+# <9259>
+# num = int(input())
+# for i in range(num):
+#     a, b = map(int, input().split())
+#     print("Case {}: {}".format(i+1, a+b))
+
+# <9085>
+cnt = int(input())
+sum = 0
+for i in range(cnt):
+    num = int(input())
+    for n in range(num):
+        num_list = list(map(int, input().split()))
+    for j in num_list:
+        sum += j
+print(sum)
