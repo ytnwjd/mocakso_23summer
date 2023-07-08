51b32af4642666640953533db3225e6065fb08d4
sujeong <ysj120808@gmail.com>
2023-07-08 12:50:56 +0900

    Update Web_front_Survey

--- BaekJoon_python/python_3일차.py
+++ BaekJoon_python/python_3일차.py
@@ -25,4 +25,30 @@
 #     a, b = map(int, input().split())
 #     print("Case {}: {}".format(i+1, a+b))
 
-# <9085>
+# <11098>
+# num = int(input())
+# for _ in range(num):
+#     player = int(input())
+#     max = 0
+#     max_name = ""
+#     for _ in range(player):
+#         money, name = input().split()
+#         money = int(money)
+#         if(money > max):
+#             max = money
+#             max_name = name
+#     print(max_name)
+
+# <1546>
+# cnt = int(input())
+# score_list = list(map(int, input().split()))
+# max = max(score_list)
+# sum = 0
+# for c in range(cnt):
+#     score_list[c] = score_list[c] / max * 100
+#     sum += score_list[c]
+# print(sum/cnt)
+
+# <1408>
+h1, m1, s1 = map(int, input().split(":"))
+h2, m2, s2 = map(int, input().split(":"))
