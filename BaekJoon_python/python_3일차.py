7bb7ef9655aa4968f6af630e26b5455767e3bfed
sujeong <ysj120808@gmail.com>
2023-07-09 22:22:29 +0900

    Add Web_front_7일차

--- BaekJoon_python/python_3일차.py
+++ BaekJoon_python/python_3일차.py
@@ -49,6 +49,73 @@
 #     sum += score_list[c]
 # print(sum/cnt)
 
-# <1408>
-h1, m1, s1 = map(int, input().split(":"))
-h2, m2, s2 = map(int, input().split(":"))
+# <5565>
+# total = int(input())
+# for _ in range(9):
+#     total -= int(input())
+# print(total)
+
+# <10178>
+# cnt = int(input())
+# for i in range(cnt):
+#     a, b = map(int, input().split())
+#     num_1 = a//b
+#     num_2 = a%b
+#     print(f"You get {a//b} piece(s) and your dad gets {a%b} piece(s).")
+
+# <2475>
+# num_list = list(map(int, input().split()))
+# sum = 0
+# for i in num_list:
+#     sum += (i*i)
+# print(sum%10)
+
+# <2455>
+# sum = 0
+# sum_li = []
+# for _ in range(4):
+#     i, o = map(int, input().split())
+#     sum = sum-i+o
+#     sum_li.append(sum)
+# print(max(sum_li))
+
+# <2506>
+# num = int(input())
+# cor_list = list(map(int, input().split()))
+# sum = 0
+# score = 1
+# for i in range(num):
+#     if(cor_list[i] == 1):
+#         sum += score
+#         score += 1
+#     if(cor_list[i]==0):
+#         score = 1
+# print(sum)
+
+# <2010>
+# import sys
+# cnt = sys.stdin.readline()
+# cnt = int(cnt)
+# total = 0
+# for _ in range(cnt):
+#     total += int(sys.stdin.readline())
+# print(total - (cnt-1))
+
+# <2442>
+# cnt = int(input())
+# for i in range(1, cnt+1):
+#     print(" " * (cnt-i) + "*" * (2*i-1))
+
+# <10984>
+cnt = int(input())
+for i in range(cnt):
+    num = int(input())
+    sum_c = 0
+    c_mul_g = 0.0
+    for j in range(num):
+        c, g = input().split()
+        c = int(c)
+        g = float(g)
+        sum_c += c
+        c_mul_g += float(c*g)
+    print("{} {.1f}".format(sum_c, c_mul_g/sum_c))
--- README.md
+++ README.md
@@ -9,6 +9,7 @@ Web_front_3일차 (2023.07.05) <br>
 Web_front_4일차 (2023.07.06) <br>
 Web_front_5일차 (2023.07.07) <br>
 Web_front_6일차 (2023.07.08) <br>
+Web_front_7일차 (2023.07.09) <br>
 
 <br>
 
