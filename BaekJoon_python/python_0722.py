d6cb607587e83abd94aee535ae00200e2a703c6f
sujeong <ysj120808@gmail.com>
2023-07-24 20:42:05 +0900

    실전코딩 React-Spring 통합코드 흐름 정리

---
+++ BaekJoon_python/python_0722.py
@@ -0,0 +1,17 @@
+# <2445>
+# n = int(input())
+# for i in range(1, n+1):
+#     print("*"*(i)+" "*(2*n-2*i)+"*"*(i))
+# for i in range(n-1, 0, -1):
+#     print("*"*(i)+" "*(2*n-2*i)+"*"*(i))
+
+# <10039>
+# sum = 0
+# for i in range(5):
+#     grade = int(input())
+#     if(grade < 40):
+#         grade = 40
+#     sum += grade
+# print(int(sum/5))
+
+# <>
